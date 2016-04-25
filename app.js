930d02a7e12bf1398253a0ca1522943e1c5ab7f6
Mack Teng <teng.mack@gmail.com>
2016-04-25 07:35:03 -0700

    Removed unnecessary template route

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public/'));
+//app.use('/', routes);
 app.use('/users', users);
 app.use('/api', api);
 
