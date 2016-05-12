8902ab945792d5fca5e35df7fa8c64d096bfd168
Mack Teng <teng.mack@gmail.com>
2016-05-12 09:50:22 -0700

    Implemented function to add events to event histories

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -14,31 +14,31 @@ module.exports.getEventHistory = function(req, res){
   }
   Patent
     .findById(req.params.patentid)
+		.populate('eventHistory')
     .select('eventHistory')
     .exec(function(err, patent){
-      if(err){
-        return sendJsonResponse(res, err, 400);
-      }
-      EventHistory
-        .findById(patent.eventHistory)
-        .exec(function(err, eventHistory){
-            if(err){
-              return sendJsonResponse(res, err, 400);
-            }
-            sendJsonResponse(res, eventHistory.eventHistory, 200);
-        });
+      if(err) return sendJsonResponse(res, err, 400);
+      sendJsonResponse(res, patent.eventHistory, 200);
     });
 };
 
 // add Event to Event History Container
 module.exports.addEvent = function(req, res){
-  if(!req.params || !res.params.patentid || !mongoose.Types.ObjectId.isValid(req.params.patentid)){
+  if(!req.params || !req.params.patentid || !mongoose.Types.ObjectId.isValid(req.params.patentid)){
 		return sendJsonResponse(res, "No PatentId Specified", 400);
 	}
-
-
-
-
+	Patent
+		.findById(req.params.patentid)
+		.populate('eventHistory')
+		.select('eventHistory')
+		.exec(function(err, patent){
+				if(err) return sendJsonResponse(res, err, 400);
+				patent.eventHistory.eventHistory.push(req.body.event);
+				patent.eventHistory.save(function(err, event){
+							if(err) return sendJsonResponse(res, err, 400);
+							sendJsonResponse(res, event, 200);
+						})
+				});
 };
 
 // delete Event from Event History Container
