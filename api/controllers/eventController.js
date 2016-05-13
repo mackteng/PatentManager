7b848fb590be5858a105598707c6a79832be0f9d
Mack Teng <teng.mack@gmail.com>
2016-05-13 07:51:36 -0700

    Implemented function to delete event from eventHistory

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -34,20 +34,30 @@ module.exports.addEvent = function(req, res){
 		.exec(function(err, patent){
 				if(err) return sendJsonResponse(res, err, 400);
 				patent.eventHistory.eventHistory.push(req.body.event);
-				patent.eventHistory.save(function(err, event){
+				patent.eventHistory.save(function(err, history){
 							if(err) return sendJsonResponse(res, err, 400);
-							sendJsonResponse(res, event, 200);
+							sendJsonResponse(res, history, 200);
 						})
 				});
 };
 
 // delete Event from Event History Container
 module.exports.deleteEvent = function(req, res){
-  if(!req.params || !res.params.patentid || !mongoose.Types.ObjectId.isValid(req.params.patentid)){
+  if(!req.params || !req.params.patentid || !mongoose.Types.ObjectId.isValid(req.params.patentid)){
 		return sendJsonResponse(res, "No PatentId Specified", 400);
 	}
 
-
-
-
+	PatentId
+		.findById(req.params.patentid)
+		.populate('eventHistory')
+		.select('eventHistory')
+		.exec(function(err, patent){
+			if(err) return sendJsonResponse(res, err, 400);
+			var history = patent.eventHistory.eventHistory;
+			history.splice(history.indexOf(req.body.event), 1);
+			patent.eventHistory.save(function(err, history){
+					if(err) return sendJsonResponse(res, err, 400);
+					sendJsonResponse(res, history, 200);
+			});
+		});
 };
