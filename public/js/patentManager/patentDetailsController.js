785b8e441c8974199cea95dc3bc7faee63c837af
Mack Teng <teng.mack@gmail.com>
2016-06-16 07:02:02 -0700

    Implemented Patent Details View

---
+++ public/js/patentManager/patentDetailsController.js
@@ -0,0 +1,105 @@
+angular
+  .module('patentApp')
+  .controller('patentDetailsController', patentDetailsController);
+
+patentDetailsController.$inject = ['$stateParams', 'patent', 'eventHistory', 'eventService', 'patentService'];
+
+function patentDetailsController($stateParams, patent, eventHistory, eventService, patentService){
+  var vm = this;
+  vm.patents = patent.allPatents;
+  vm.patent = null;
+  vm.eventHistory = eventHistory.data.eventHistory;
+
+
+  for(var i = 0; i < vm.patents.length; i++){
+    if(vm.patents[i]._id == $stateParams.id){
+      vm.patent = vm.patents[i];
+      break;
+    }
+  }
+
+  // format date
+  vm.patent.filingDate = new Date(vm.patent.filingDate);
+  if(vm.patent.priority){
+    vm.patent.priority.priorityDate = new Date(vm.patent.priority.priorityDate);
+  }
+
+  for(i = 0; i < vm.eventHistory.length; i++){
+    vm.eventHistory[i].eventDeadline = new Date(vm.eventHistory[i].eventDeadline);
+  }
+
+  vm.enablePriorityForm = vm.patent.priority!=null;
+  //list of countries
+  vm.listCountries = ['US', 'TW', 'CN', 'JP', 'KR', 'EU'];
+  // application Types
+  vm.applicationTypes = ['Patent'];
+  // date picker
+  vm.picker1 = false;
+  vm.picker2 = false;
+  vm.picker3 = false;
+  vm.dateOptions = {};
+  vm.openDatePicker1 = function(){
+    vm.picker1 = true;
+  }
+  vm.openDatePicker2 = function(){
+    vm.picker2 = true;
+  }
+  vm.openDatePicker3 = function(){
+    vm.picker3 = true;
+  }
+  // Inventors
+  vm.addInventor = function(){
+    vm.patent.inventors.push({});
+  };
+  vm.deleteInventor = function($index){
+    vm.patent.inventors.splice($index, 1);
+  };
+
+  vm.newEvent = {};
+
+  // Timeline options
+  vm.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+
+  // Edit option
+  vm.editEnabled = false;
+  vm.oldPatent = null;
+  vm.enableEdit = function(){
+    vm.oldPatent = angular.copy(vm.patent);
+    vm.editEnabled = true;
+  }
+
+  vm.cancelEdit = function(){
+    angular.copy(vm.oldPatent, vm.patent);
+    vm.editEnabled = false;
+  }
+
+  vm.save = function(){
+    console.log(vm.patent);
+    patentService
+      .updatePatent(vm.patent, vm.patent._id)
+      .success(function(){
+        angular.copy(vm.patent, vm.oldPatent);
+        vm.editEnabled = false;
+      })
+      .error(function(){
+        alert('error');
+      });
+  }
+
+  vm.addEvent = function(){
+    console.log(vm.newEvent);
+    eventService
+      .addEvent(vm.patent._id, vm.newEvent)
+      .success(function(){
+        vm.eventHistory.unshift(vm.newEvent);
+        vm.newEvent = null;
+      })
+      .error(function(){
+        alert('error');
+      });
+  };
+}
--- public/js/patentManager/patentManagerController.js
+++ public/js/patentManager/patentManagerController.js
@@ -1,20 +1,20 @@
 angular.module('patentApp').controller('patentController', patentController);
-angular.module('patentApp').controller('newPatentFormController', newPatentFormController);
-
 patentController.$inject=['allPatents', 'allClients', '$uibModal', '$log'];
-newPatentFormController.$inject=['$uibModalInstance', 'allClients', 'patentService'];
-
 function patentController(allPatents, allClients, $uibModal){
   var vm = this;
   // patents
   vm.patents = allPatents.data;
   vm.clients = allClients.data;
 
+
+  console.log(vm.patents);
+
   // advanced search collapse variable and functions
   vm.advancedSearchCollapse = true;
   vm.toggleAdvancedSearch = function(){
     vm.advancedSearchCollapse = !vm.advancedSearchCollapse;
   }
+  vm.search={};
 
   // new patent form variable and functions
   vm.newPatentForm = function(){
@@ -35,83 +35,4 @@ function patentController(allPatents, allClients, $uibModal){
       vm.patents.push(patent);
     });
   }
-}
-
-function newPatentFormController($uibModalInstance,  allClients, patentService){
-  var vm = this;
-
-  // patent application
-  vm.patent = {};
-  vm.clients = allClients;
-  // message alert
-  vm.message = {
-    type: 'success',
-    content: null
-  }
-  vm.closeMessage = function(){
-    vm.message.content = null;
-  }
-  vm.setMessage = function(type,message){
-    vm.message.type = type;
-    vm.message.content = message;
-  }
-
-  // active on by default
-  vm.patent.active = true;
-
-  // date picker
-  vm.picker1 = false;
-  vm.picker2 = false;
-  vm.dateOptions = {};
-  vm.openDatePicker1 = function(){
-    vm.picker1 = true;
-  }
-  vm.openDatePicker2 = function(){
-    vm.picker2 = true;
-  }
-
-  // application Types
-  vm.applicationTypes = ['Patent'];
-
-  // enable Priority Form
-  vm.enablePriorityForm = false;
-  vm.clearPriority = function(){
-    delete vm.patent.priority;
-  }
-
-  // Inventors
-  vm.patent.inventors = [];
-  vm.addInventor = function(){
-    vm.patent.inventors.push({});
-  };
-  vm.deleteInventor = function($index){
-    vm.patent.inventors.splice($index, 1);
-  };
-
-  // List of countries
-  vm.listCountries = ['US', 'TW', 'CN', 'JP', 'KR', 'EU'];
-
-  // Submit patent application
-  vm.submit = function(){
-    //alert(JSON.stringify(vm.patent, null, 4));
-    // validate
-    if(!vm.clientId || !vm.docketNumber || !vm.country || !vm.filingDate || !vm.filingNumber || !vm.applicationType || !vm.englishTitle){
-      vm.setMessage('danger', 'Please fill in the required fields!');
-      return false;
-    }
-    // send
-    patentService
-      .addNewPatent(vm.patent)
-      .success(function(data){
-        $uibModalInstance.close(data);
-      })
-      .error(function(err){
-        vm.setMessage('danger', 'ERROR  : ' + err.message);
-      });
-  };
-
-  // cancel
-  vm.cancel = function(){
-    $uibModalInstance.dismiss('cancel');
-  };
 }
