f1bc9053c63e384ee4eba0d61d59d9f39b9ec0e2
Mack Teng <teng.mack@gmail.com>
2016-08-14 22:39:21 +0800

    Fixed undefined email due to closures

--- services/notification/notification.js
+++ services/notification/notification.js
@@ -6,16 +6,18 @@ var table = {};
 
 var generateEmailFunction = function(event, emailAddress){
   return function(){
+    console.log('here');
     var helper = require('sendgrid').mail
     var date = new Date(event.eventDeadline);
-    var from_email = new helper.Email("service@litron-intl.com");
+    var from_email = new helper.Email("deadline@litron-intl.com");
     var to_email = new helper.Email(emailAddress);
-    var subject = "This is a reminder that the deadline : " + event.eventName + " is approaching on : " + new Date(event.eventDeadline);
-    var content = new helper.Content("text/plain", event.eventNote);
+    var subject = "Deadline : '" + event.eventName + "' is approaching on : " + new Date(event.eventDeadline).toDateString();
+    var content = new helper.Content("text/plain", 'Comments: '+event.eventNote);
     var mail = new helper.Mail(from_email,subject,to_email,content);
     var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
     var requestBody = mail.toJSON();
     var request = sg.emptyRequest();
+    console.log(to_email);
     request.method = 'POST';
     request.path = '/v3/mail/send';
     request.body = requestBody;
@@ -23,6 +25,7 @@ var generateEmailFunction = function(event, emailAddress){
       if(err){
         console.log(err);
       }
+      console.log(response);
     });
   }
 }
@@ -34,12 +37,15 @@ var scheduleEvent = function(event){
       table[event._id] = [];
     }
     for(var j = 0; j < event.notificationDates.length; j++){
+      if(new Date(event.notificationDates[j]) < new Date()) continue;
       for(var k = 0; k < event.notificationEmails.length; k++){
         console.log('Scheduling Job For Event ' + event._id + ' for ' + event.notificationEmails[k] + ' on ' + event.notificationDates[j]);
-        var job = schedule.scheduleJob(new Date(event.notificationDates[j]), function(){
-          generateEmailFunction(event, event.notificationEmails[k])();
-        });
-        table[event._id].push(job);
+        var jobFunction = function(email){
+          return schedule.scheduleJob(new Date(event.notificationDates[j]), function(){
+            generateEmailFunction(event, email)();
+          });
+        }
+        table[event._id].push(jobFunction(event.notificationEmails[k]));
       }
     }
   }
@@ -59,7 +65,11 @@ var populateTable = function(){
     });
 };
 
-module.exports.updatePatent = function(eventid){
+module.exports.addEvent = function(event){
+  scheduleEvent(event);
+}
+
+module.exports.updateEvent = function(eventid){
 
 
 
